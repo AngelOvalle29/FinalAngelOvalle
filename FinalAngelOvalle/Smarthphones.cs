d214a1bb730ac44d3e2637477dbaeef8a3a9b01e
Angel Ovalle <angelitoovalle10barsa@gmail.com>
2024-06-06 09:04:05 -0600

    Agregar archivos de proyecto.

---
+++ FinalAngelOvalle/Smarthphones.cs
@@ -0,0 +1,16 @@
+﻿namespace FinalAngelOvalle
+{
+    public class Smarthphones : Dispositivos
+    {
+        public string SOperativo { get; set; }
+
+        public string NumCamara {  get; set; }
+
+        public Smarthphones()
+        {
+            SOperativo =string.Empty;
+            NumCamara = string.Empty;  
+        }
+
+    }
+}
